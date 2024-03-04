d8bed53c14e2db94a528753e1f649e8f26291711
Samin010 <saminbashir484@gmail.com>
2024-03-04 19:45:36 +0530

    .

---
+++ src/Components/Accordian.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import data from "./data.js";
+function Accordian() {
+  const [selectedId, setSelectedId] = useState(null);
+  const [multipleSelection, setMultipleSelection] = useState(false);
+  const [multipleArr, setMultipleArr] = useState([]);
+  const enableMultiSelection = () => {
+    console.log(multipleSelection);
+    setMultipleSelection(!multipleSelection);
+  };
+  const handleSingleSelection = (id) => {
+    setSelectedId(selectedId === id ? null : id);
+  };
+  const handleMultiSelection = (id) => {
+    let copyArr = [...multipleArr];
+    const index = copyArr.indexOf(id);
+    console.log(index);
+    if (index === -1) copyArr.push(id);
+    else copyArr.splice(index, 1);
+    setMultipleArr(copyArr);
+  };
+
+  return (
+    <>
+      <button onClick={enableMultiSelection}>Enable Multi-Selection</button>
+      {data.map((item) => {
+        return (
+          <div
+            key={item.id}
+            onClick={
+              multipleSelection === true
+                ? () => handleMultiSelection(item.id)
+                : () => handleSingleSelection(item.id)
+            }
+          >
+            <h3>{item.question}</h3>
+            {multipleSelection === true
+              ? multipleArr.indexOf(item.id) !== -1 && <div>{item.answer}</div>
+              : selectedId === item.id && <div>{item.answer}</div>}
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default Accordian;
---
+++ src/Components/data.js
@@ -0,0 +1,23 @@
+let  data=[
+  {
+    id:"1",
+    question:"What is your name?",
+    answer:"My name is , Samin"
+  },
+  {
+    id:"2",
+    question:"What is your name?",
+    answer:"My name is , Samin"
+  },
+  {
+    id:"3",
+    question:"What is your name?",
+    answer:"My name is , Samin"
+  },
+  {
+    id:"4",
+    question:"What is your name?",
+    answer:"My name is , Samin"
+  },
+]
+export default data
